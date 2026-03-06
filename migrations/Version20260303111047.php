d3f0085141abb8dde3b63f1835dae2ef579743a4
Titov Alexander <alex.t@truelabel.io>
2026-03-06 11:02:18 +0300

    Добавлена миграция

---
+++ migrations/Version20260303111047.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260303111047 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comment_comments (id UUID NOT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, author_id UUID NOT NULL, text VARCHAR(255) NOT NULL, update_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, entity_type VARCHAR(255) NOT NULL, entity_id VARCHAR(36) NOT NULL, PRIMARY KEY (id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE comment_comments');
+    }
+}
